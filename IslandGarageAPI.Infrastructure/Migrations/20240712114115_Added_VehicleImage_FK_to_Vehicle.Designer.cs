4fb7182c26f200087afdfe9893542de15411bd9e
Sebastien Larue <sebastienlar619@gmail.com>
2024-07-12 15:43:09 +0400

    Updated the Vehicles Table with new FK

--- IslandGarageAPI.Infrastructure/Migrations/20240712114115_Added_VehicleImage_FK_to_Vehicle.Designer.cs
+++ IslandGarageAPI.Infrastructure/Migrations/20240712114115_Added_VehicleImage_FK_to_Vehicle.Designer.cs
@@ -4,6 +4,7 @@ using IslandGarageAPI.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IslandGarageAPI.Infrastructure.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240712114115_Added_VehicleImage_FK_to_Vehicle")]
+    partial class Added_VehicleImage_FK_to_Vehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IslandGarageAPI.Infrastructure/Migrations/20240712114115_Added_VehicleImage_FK_to_Vehicle.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IslandGarageAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_VehicleImage_FK_to_Vehicle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleImageId",
+                table: "Vehicles",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicles_VehicleImageId",
+                table: "Vehicles",
+                column: "VehicleImageId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_VehicleImages_VehicleImageId",
+                table: "Vehicles",
+                column: "VehicleImageId",
+                principalTable: "VehicleImages",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_VehicleImages_VehicleImageId",
+                table: "Vehicles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vehicles_VehicleImageId",
+                table: "Vehicles");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VehicleImageId",
+                table: "Vehicles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
